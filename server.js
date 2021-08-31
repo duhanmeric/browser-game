e88251b2f224c0f890f29163a3c2f34485b1afbd
Duhan Meric <duhanmeric@gmail.com>
2021-08-31 17:42:29 +0300

    deploy

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
   },
 });
+
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => {
   res.send("Hello World! asdasd");
