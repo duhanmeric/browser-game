a5ce5f5b921d20f9b9e0bc8966f976714d00d262
Duhan Meric <duhanmeric@gmail.com>
2021-08-31 17:11:49 +0300

    last fixes

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Login({ userName, setUserName, setScene }) {
+  const [error, setError] = useState(false);
+
   const handleUserName = (e) => {
     setUserName(e.target.value);
   };
@@ -8,20 +10,28 @@ export default function Login({ userName, setUserName, setScene }) {
   const login = () => {
     if (userName !== "") {
       if (userName.length >= 3 && userName.length <= 10) setScene(1);
+      else setError(true);
     }
   };
 
   return (
     <div id="login">
-      <input
-        value={userName}
-        type="text"
-        placeholder="Enter your nickname"
-        onChange={(e) => handleUserName(e)}
-      />
-      <button id="loginBtn" onClick={() => login()}>
-        Login
-      </button>
+      {error ? (
+        <div id="usernameError">
+          Username characters count must be between 3-10
+        </div>
+      ) : null}
+      <div id="loginContainer">
+        <input
+          value={userName}
+          type="text"
+          placeholder="Enter your nickname"
+          onChange={(e) => handleUserName(e)}
+        />
+        <button id="loginBtn" onClick={() => login()}>
+          Login
+        </button>
+      </div>
     </div>
   );
 }
